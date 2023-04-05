09baba0f533f39563a3929f4d877474fb89b8ed8
putrinthsya <129546694+putrinthsya@users.noreply.github.com>
2023-04-05 19:49:25 +0700

    Add files via upload

---
+++ estimasi_Bikes.py
@@ -0,0 +1,17 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_bikes.sav', 'rb'))
+
+kms_driven = st.number_input('Input Jarak Tempuh')
+age = st.number_input('Input Umur Sepeda')
+power = st.number_input('Input Kecepatan Sepeda')
+
+predict = ' '
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[kms_driven, age, power]]
+    )
+    st.write ('Estimasi Harga Sepeda : ', predict)
+    
